1db7f96d4471516176795216fd26a8a251f0f5cb
Craig McNamara <cmcnamara87@gmail.com>
2016-05-07 14:23:13 +1000

    feat(nav bar): add link to bargain bin

--- resources/views/toys/index.blade.php
+++ resources/views/toys/index.blade.php
@@ -135,6 +135,14 @@
 <nav class="navbar navbar-dark navbar-full bg-primary">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">ToyFuel</a>
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ url('/') }}">Trending</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('bargain-bin') }}">Bargain Bin</a>
+            </li>
+        </ul>
         <ul class="nav navbar-nav pull-xs-right">
             {{--<li class="nav-item active">--}}
                 {{--<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
